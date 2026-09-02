913acabdfb829b8f37ef63fd360f697e11befad6
Drstyrofoam <isaac.legham@gmail.com>
2026-09-02 15:05:38 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@ self.addEventListener('push', function (event) {
   event.waitUntil(
     self.registration.showNotification(data.title || 'Prayer reminder', {
       body: data.body || '',
-      icon: '/icon.png',
-      badge: '/icon.png',
+      icon: '/icon-192.png',
+      badge: '/icon-192.png',
     })
   );
 });
